16be609ef9cb982566961dc3670c8388b4bd271f
tburtenshaw <tburtenshaw@422d8566-1793-11df-871c-7de3c3ae1d82>
2010-02-19 11:58:27 +0000

    More on list display. Getting info window started - displays description.

--- trunk/info.h
+++ trunk/info.h
@@ -1,8 +1,50 @@
 #include <windows.h>
 #include <windowsx.h>
 
+typedef struct sInfoFileInformation INFOFILE_INFO;
+
+struct sInfoFileInformation
+{
+	char filename[MAX_PATH];
+
+	//These are identical to the list vars
+	char recordingname[120];
+	long unixtime;				//From the unix time (loaded, not calculated)
+	unsigned long duration;		//the duration in seconds as loaded
+	unsigned long filesize;		//the file size as loaded
+
+	char description[512];		//Very comfortable number of bytes
+
+	int decimalbyteHour;
+	int decimalbyteMinute;
+
+	char assocMpeg[256];
+	char assocNav[256];
+	char assocJpeg[256];
+
+	//PIDs
+	int PID[8];			//Arbitary - I don't think any of ours have eight PIDs
+
+	//All these things happen twice - prefix dup
+	long dup_unixtime;				//From the unix time (loaded, not calculated)
+	unsigned long dup_duration;		//the duration in seconds as loaded
+	unsigned long dup_filesize;		//the file size as loaded
+
+	int dup_decimalbyteHour;
+	int dup_decimalbyteMinute;
+
+
+};
+
 int InfoWindowRegisterWndClass(HINSTANCE hInst);
 HWND InfoWindowCreateOrShow(HWND hwnd, HWND hwndChild, HINSTANCE hInst);
 HWND InfoWindowCreate(HWND hwndMDIClient, HINSTANCE hInst);
+int  InfoWindowLoadFile(HWND hwnd, char *filename);
+
+
+
+
+
+
 LRESULT CALLBACK ChildWndInfoProc(HWND hwnd,UINT msg,WPARAM wParam,LPARAM lParam);
 
--- trunk/list.c
+++ trunk/list.c
@@ -99,7 +99,7 @@ LRESULT CALLBACK ChildWndListProc(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam
 	RECT clientRect;
 
 	switch(msg) {
-		case WM_CREATE:		//When we create the list we do certain things
+		case WM_CREATE:		//When we create the list we do certain things (having this in a message may create race condition - need to fix urgently
 			hInst=((LPCREATESTRUCT)lparam)->hInstance;
 
 			lpListWindowInfo=malloc(sizeof(LISTWINDOW_INFO));	//make the structure that holds child windows and folderlist
@@ -160,6 +160,13 @@ LRESULT CALLBACK ListChildFileProc(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lpara
 			lpListWindowInfo=(LISTWINDOW_INFO *)GetWindowLong(GetParent(hwnd), GWL_USERDATA);
 			PaintListFileWindow(hwnd, lpListWindowInfo);
 			break;
+		case WM_ERASEBKGND:
+			return 1;
+			break;
+		case WM_SIZE:
+			InvalidateRect(hwnd, NULL, FALSE);
+			break;
+
 	}
 	return DefMDIChildProc(hwnd, msg, wparam, lparam);
 }
@@ -230,8 +237,13 @@ int PaintListFileWindow(HWND hwnd, LISTWINDOW_INFO *lpListWindowInfo)
 	RECT clientRect;
 	RECT textRect;
 
+	TEXTMETRIC textMetric;
 	HFONT hLargeFont;
 	HFONT hSmallFont;
+
+	int	heightLargeFont;
+	int heightSmallFont;
+	int	heightLine;
 
 	HDC hdc;
 	PAINTSTRUCT psPaint;
@@ -243,51 +255,113 @@ int PaintListFileWindow(HWND hwnd, LISTWINDOW_INFO *lpListWindowInfo)
 	FILETIME filetime;
 	SYSTEMTIME systemtime;
 
+
+	int smallcolumnwidth=80;
+	int dividerlinewidth=2;
 	int y=0;
+
 
 	lpDirectoryInfo=&lpListWindowInfo->directoryInfo;
 
 	hdc = BeginPaint(hwnd, &psPaint);
 	GetClientRect(hwnd, &clientRect);
 
+	//Load the fonts
+	hLargeFont = CreateFont(
+				MulDiv(13, GetDeviceCaps(hdc, LOGPIXELSY), 72),
+				0,0,0,FW_BOLD,
+				FALSE,FALSE,FALSE,
+				DEFAULT_CHARSET,OUT_OUTLINE_PRECIS,
+                CLIP_DEFAULT_PRECIS,ANTIALIASED_QUALITY, VARIABLE_PITCH|FF_SWISS,"Arial");
+
+	hSmallFont = CreateFont(
+				MulDiv(8, GetDeviceCaps(hdc, LOGPIXELSY), 72),
+				0,0,0,FW_BOLD,
+				FALSE,FALSE,FALSE,
+				DEFAULT_CHARSET,OUT_OUTLINE_PRECIS,
+                CLIP_DEFAULT_PRECIS,ANTIALIASED_QUALITY, VARIABLE_PITCH|FF_SWISS,"Arial");
+
+
+	SelectObject(hdc,hLargeFont);
+	GetTextMetrics(hdc, &textMetric);
+	heightLargeFont= textMetric.tmHeight;
+
+	SelectObject(hdc,hSmallFont);
+	GetTextMetrics(hdc, &textMetric);
+	heightSmallFont= textMetric.tmHeight;
+
+	if ((heightLargeFont)>(2*heightSmallFont))	//if the large font is much larger, then it controls line height
+		heightLine=heightLargeFont/2;
+	else
+		heightLine=heightSmallFont;
+
+
+
 	llist=lpDirectoryInfo->first;
 	while ((llist)&&(y<clientRect.bottom))	{
 		SetBkColor(hdc, RGB_ZINNY_MIDPURPLE);
 
-		textRect.top=y;textRect.bottom=y+20;
-		textRect.left=clientRect.left; textRect.right=clientRect.right;
-		ExtTextOut(hdc, 5,textRect.top, ETO_OPAQUE, &textRect, llist->shortfilename, strlen(llist->shortfilename), NULL);
 
-		textRect.top=y+20;textRect.bottom=y+40;
-		textRect.left=clientRect.left; textRect.right=clientRect.right;
+		//Draw the Recording Name, then the filename (on the left)
+		SetTextAlign(hdc, TA_LEFT);
+		SelectObject(hdc,hLargeFont);
+
+		textRect.left=clientRect.left; textRect.right=clientRect.right-smallcolumnwidth;
+		//textRect.top=y+0;textRect.bottom=y+2*heightLine;
+		textRect.top=y+0;textRect.bottom=y+heightLargeFont;
 		ExtTextOut(hdc, 5,textRect.top, ETO_OPAQUE, &textRect, llist->recordingname, strlen(llist->recordingname), NULL);
 
+		SelectObject(hdc,hSmallFont);
+
+		//textRect.top=y+2*heightLine;textRect.bottom=y+3*heightLine;
+		textRect.top=y+heightLargeFont;textRect.bottom=y+3*heightLine;
+		ExtTextOut(hdc, 5,textRect.top, ETO_OPAQUE, &textRect, llist->shortfilename, strlen(llist->shortfilename), NULL);
+
+
+		SetTextAlign(hdc, TA_RIGHT);
 
 		//This helps to get a pretty date
 		UnixTimeToFileTime(llist->unixtime, &filetime);
 		FileTimeToSystemTime(&filetime, &systemtime);
+		textRect.top=y+0;textRect.bottom=y+1*heightLine;
+		textRect.left=clientRect.right-smallcolumnwidth; textRect.right=clientRect.right;
+		sprintf(textoutbuffer, "%02d/%02d/%04d",systemtime.wDay,systemtime.wMonth,systemtime.wYear);
+		ExtTextOut(hdc, textRect.right-5,textRect.top, ETO_OPAQUE, &textRect, textoutbuffer, strlen(textoutbuffer), NULL);
 
-		BytesDisplayNice(llist->filesize, "%.2f %s", 1, filesizeString);
+		textRect.top=y+1*heightLine;textRect.bottom=y+2*heightLine;
+		sprintf(textoutbuffer, "%02d:%02d",systemtime.wHour,systemtime.wMinute);
+		ExtTextOut(hdc, textRect.right-5,textRect.top, ETO_OPAQUE, &textRect, textoutbuffer, strlen(textoutbuffer), NULL);
+
+
 		DurationShortFormatDHMS(llist->duration, durationString);
+	    sprintf(textoutbuffer, "%s", durationString);
+		sprintf(textoutbuffer, "Tr %i %i", heightLargeFont, heightSmallFont);
+		textRect.top=y+2*heightLine;textRect.bottom=y+3*heightLine;
+		ExtTextOut(hdc, textRect.right-5,textRect.top, ETO_OPAQUE, &textRect, textoutbuffer, strlen(textoutbuffer), NULL);
 
-		sprintf(textoutbuffer, "%02d/%02d/%04d, Size:%s , %s", systemtime.wDay,systemtime.wMonth,systemtime.wYear , filesizeString, durationString);
 
-		textRect.top=y+40;textRect.bottom=y+60;
-		ExtTextOut(hdc, 5,textRect.top, ETO_OPAQUE, &textRect, textoutbuffer, strlen(textoutbuffer), NULL);
+		//We won't display the size, as if often not done correctly
+		//BytesDisplayNice(llist->filesize, "%.2f %s", 1, filesizeString);
+
 
 		SetBkColor(hdc, RGB_ZINNY_HIGHPURPLE);
-		textRect.top=y+60;textRect.bottom=y+62;
+		textRect.left=clientRect.left; textRect.right=clientRect.right;
+		textRect.top=y+3*heightLine;textRect.bottom=y+3*heightLine+dividerlinewidth;
 		ExtTextOut(hdc, 0,0, ETO_OPAQUE, &textRect, "", 0, NULL);	//this is the separator
 
-		y+=62;
+		y+=3*heightLine+dividerlinewidth;
 		llist=llist->next;
 	}
 
 	SetBkColor(hdc, RGB_ZINNY_MIDPURPLE);
+	textRect.left=clientRect.left; textRect.right=clientRect.right;
 	textRect.top=y; textRect.bottom=clientRect.bottom;
 	ExtTextOut(hdc, 0,0, ETO_OPAQUE, &textRect, "", 0, NULL);	//this is the separator
 
 	//SHBrowseForFolder
+
+	DeleteObject(hSmallFont);
+	DeleteObject(hLargeFont);
 
 	EndPaint (hwnd, &psPaint);
 	return 0;
